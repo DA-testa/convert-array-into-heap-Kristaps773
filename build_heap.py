88f7ca19ab435c83a6cdec2914bced4f1a167ecd
Kristaps773 <84283476+Kristaps773@users.noreply.github.com>
2023-03-09 18:13:07 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,47 @@
+#Krists Kristaps Dūda 221RDB518 10.grupa
 # python3
 
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
+    # TO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    n = len(data)
+    for i in range(n/2,-1,-1):
+        j=i
+        while True:
+            mini = j
+            leftChild = 2*j + 1
+            rightChild = 2*j + 2
+            if leftChild < n and data[leftChild] < data[mini] :
+                mini = leftChild
+            if rightChild < n and data[rightChild] < data[mini] :
+                mini = rightChild
+            if j!=mini:
+                data[j], data[mini] = data[mini], data[j]
+                swaps.append(j, mini)
+                j =mini
+            else:
+                break
 
     return swaps
 
 
 def main():
     
-    # TODO : add input and corresponding checks
+    ##TO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    input1 = input("Enter 'i' to input from keyboard, 'f' to input from file: ")
+    if input1.lower() == 'f':
+        filename = input("File path: ")
+        with open(f'{filename}') as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+    elif input1.lower() == 'i':
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+        n = int(input())
+        data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
@@ -28,7 +50,7 @@ def main():
     # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
+    ##T: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
