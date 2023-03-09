0af27e97afeb1a4dcc3a3be490c7ee900bb6d052
Kristaps773 <84283476+Kristaps773@users.noreply.github.com>
2023-03-09 19:29:39 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,37 +2,12 @@
 # python3
 
 
-def build_heap(data):
-    swaps = []
-    # TO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-    n = len(data)
-    for i in range(n//2,-1,-1):
-        j=i
-        while True:
-            mini = j
-            leftChild = 2*j + 1
-            rightChild = 2*j + 2
-            if leftChild < n and data[leftChild] < data[mini] :
-                mini = leftChild
-            if rightChild < n and data[rightChild] < data[mini] :
-                mini = rightChild
-            if j!=mini:
-                data[j], data[mini] = data[mini], data[j]
-                swaps.append((j, mini))
-                j = mini
-            else:
-                break
-
-    return swaps
-
-
 def main():
 
     ##TO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-    input1 = input()
+    input1 = input("Enter 'i' to input from keyboard, 'f' to input from file: ")
     if input1.lower() == 'f':
         filename = input("File path: ")
         with open(f'{filename}') as f:
@@ -62,3 +37,28 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def build_heap(data):
+    swaps = []
+    # TO: Creat heap and heap sort
+    # try to achieve  O(n) and not O(n2)
+    n = len(data)
+    for i in range(n//2,-1,-1):
+        j=i
+        while True:
+            mini = j
+            leftChild = 2*j + 1
+            rightChild = 2*j + 2
+            if leftChild < n and data[leftChild] < data[mini] :
+                mini = leftChild
+            if rightChild < n and data[rightChild] < data[mini] :
+                mini = rightChild
+            if j!=mini:
+                data[j], data[mini] = data[mini], data[j]
+                swaps.append((j, mini))
+                j = mini
+            else:
+                break
+
+    return swaps
+
