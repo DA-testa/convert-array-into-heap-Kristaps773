a9adb5dee5edad12e419754c3f9d604f44144fb9
Kristaps773 <84283476+Kristaps773@users.noreply.github.com>
2023-03-10 15:28:11 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -12,18 +12,15 @@ def build_heap(data):
         j=i
         while True:
             rightChild = j*2+2
-            leftChild = 2*2+1
+            leftChild = j*2+1
             mini = j
-            if rightChild<size and data[rightChild]<data[mini]:
+            if rightChild<size and data[rightChild]<data[leftChild]:
                 mini = rightChild
-            if leftChild<size and data[leftChild]<data[mini]:
-                mini = leftChild
-            if mini !=j:
-                swaps.append((j, mini))
-                data[j], data[mini] = data[mini], data[j]
-                j = mini
-            else:
+            if data[j] <= data[mini]:
                 break
+            swaps.append((j,mini))
+            data[j], data[mini] = data[mini], data[j]
+            j= mini
     return swaps
 
 
@@ -34,27 +31,19 @@ def main():
     # add another input for leftjor F 
     # first two tests are from keyboard, third test is from a file
     input1 = input()
+    if 'I' in input1:
+        n = int(input())
+        data = list(map(int, input().split()))
     if 'F' in input1:
         file = input()
-        file = "test/" + file
+        #file = "test/" + file
         try:
             with open(file, mode = "r") as f:
                     n = int(f.readline())
                     data = list(map(int, f.readline().split()))
-                    swaps = build_heap(data)
-                    print(len(swaps))
-                    for i, j in swaps:
-                        print(i, j)
+                    assert len(data) == n
         except FileNotFoundError:
-            return
-        
-    elif 'I' in input1:
-        n = int(input())
-        data = list(map(int, input().split()))
-        swaps = build_heap(data)
-        print(len(swaps))
-        for i, j in swaps:
-            print(i, j)
+            return  
     else:
         return
     
@@ -64,16 +53,16 @@ def main():
     
     # calls function to assess the data,
     # and give back all swaps
-    #swaps = build_heap(data)
+    swaps = build_heap(data)
 
     ##T: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
 
 
     # output all swaps
-    #print(len(swaps))
-    #for i, j in swaps:
-    #    print(i, j)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
 
 if __name__ == "__main__":
